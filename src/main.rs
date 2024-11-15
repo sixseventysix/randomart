bcc9ffb66a58d2c727300379779c66f9751bc799
samarth <samarthkulkarni@icloud.com>
2024-11-15 08:45:14 +0530

    put assert inside extract channels from triple

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use randomart::{utils::{ fnv1a, render_pixels, PixelCoordinates }, Grammar, Node};
+use randomart::{utils::{ fnv1a, render_pixels, PixelCoordinates }, Grammar};
 use std::env;
 
 fn main() {
@@ -11,7 +11,7 @@ fn main() {
 
     let string = args[1].clone();
     let depth: u32 = args[2].parse().unwrap_or_else(|_| {
-        eprintln!("Error: depth must be a positive integer");
+        eprintln!("error: depth must be a positive integer");
         std::process::exit(1);
     });
     let output_filename = args[3].clone();
@@ -20,18 +20,9 @@ fn main() {
     
     let start_rule = 0;
     let generated_node = grammar.gen_rule(start_rule, depth).unwrap();
-
-    assert!(
-        matches!(*generated_node, Node::Triple(_, _, _)),
-        "expected the generated node to be a Node::Triple, but found: {:?}",
-        generated_node
-    );
-    // unsafe reason:
-    // you must give extract_channels_from_triple a Node::Triple for it to extract the r, g, b channels
-    // if extract_channels_from_triple returns None that means the node is not a Node::Triple
-    // above assert checks for that
-    let (r_str, g_str, b_str) = unsafe { generated_node.extract_channels_from_triple().unwrap_unchecked() };
+    let (r_str, g_str, b_str) = generated_node.extract_channels_from_triple();
     println!("R:{}\n\nG:{}\n\nB:{}", r_str, g_str, b_str);
+
     let rgb_function = |coords: PixelCoordinates| {
         generated_node.eval_rgb(coords.x, coords.y)
     };
