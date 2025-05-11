47ad4e4731b15588858d19f047edcac27e30fed8
samarth <samarthkulkarni@icloud.com>
2025-05-11 19:04:19 +0530

    comparison bw stack_based and closure_trees

--- benches/a.rs
+++ benches/a.rs
@@ -3,11 +3,11 @@ use criterion::{criterion_group, criterion_main};
 use randomart::{ ClosureTree, Grammar };
 use randomart::utils::{ render_pixels, PixelCoordinates, fnv1a };
 
-pub fn bench_render_pixels(c: &mut Criterion) {
+pub fn bench_render_pixels_stack_based(c: &mut Criterion) {
     let string = "spiderman 1";
-    let depth = 40;
-    let width = 400;
-    let height = 400;
+    let depth = 60;
+    let width = 100;
+    let height = 100;
 
     let seed = fnv1a(&string);
     let mut grammar = Grammar::default(seed);
@@ -34,12 +34,34 @@ pub fn bench_render_pixels(c: &mut Criterion) {
         b: b_instructions,
     };
     let rgb_fn = program.to_fn();
-    c.bench_function("render_pixels", |b| {
+    c.bench_function("render_pixels_stack", |b| {
         b.iter(|| {
             black_box(render_pixels(&rgb_fn, width, height));
         });
     });
 }
 
-criterion_group!(benches, bench_render_pixels);
+pub fn bench_render_pixels_closure_tree(c: &mut Criterion) {
+    let string = "spiderman 1";
+    let depth = 60;
+    let width = 100;
+    let height = 100;
+
+    let seed = fnv1a(&string);
+    let mut grammar = Grammar::default(seed);
+
+    let mut generated_node = grammar.gen_rule(0, depth).unwrap();
+    generated_node.simplify_triple();
+
+    let closure_tree = ClosureTree::from_node(&generated_node);
+
+    let rgb_fn = move |coord: PixelCoordinates| closure_tree.eval_rgb(coord.x, coord.y);
+    c.bench_function("render_pixels_closure_tree", |b| {
+        b.iter(|| {
+            black_box(render_pixels(&rgb_fn, width, height));
+        });
+    });
+}
+
+criterion_group!(benches, bench_render_pixels_closure_tree, bench_render_pixels_stack_based);
 criterion_main!(benches);
