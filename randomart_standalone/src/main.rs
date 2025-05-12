765937605d6d4bd5ac75d369af1660dcba48ff53
samarth <samarthkulkarni@icloud.com>
2025-05-12 13:56:38 +0530

    some refactoring

---
+++ randomart_standalone/src/main.rs
@@ -0,0 +1,56 @@
+use randomart::{utils::{ fnv1a, render_pixels, PixelCoordinates }, Grammar, ClosureTree};
+use std::{env, path::PathBuf};
+
+fn get_output_path(file_name: &str) -> PathBuf {
+    let current_dir = env::current_dir().expect("failed to get the current working directory");
+    current_dir.join(file_name)
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 4 || args.len() > 6 {
+        eprintln!("usage: {} <string> <depth> <output file path> <width>(optional) <height>(optional)", args[0]);
+        std::process::exit(1);
+    }
+
+    let string = args[1].clone();
+    let depth: u32 = args[2].parse().unwrap_or_else(|_| {
+        eprintln!("error: depth must be a positive integer");
+        std::process::exit(1);
+    });
+    let output_img_filename = format!("{}.png", args[3]);
+    let output_formula_filename = format!("{}.txt", args[3]);
+
+    let width: u32 = args.get(4).map_or(400, |arg| {
+        arg.parse().unwrap_or_else(|_| {
+            eprintln!("ERR: invalid width, must be a positive integer");
+            std::process::exit(1);
+        })
+    });
+    let height: u32 = args.get(5).map_or(400, |arg| {
+        arg.parse().unwrap_or_else(|_| {
+            eprintln!("ERR: invalid height, must be a positive integer");
+            std::process::exit(1);
+        })
+    });
+
+    let seed = fnv1a(&string);
+    let mut grammar = Grammar::default(seed);
+    
+    let start_rule = 0;
+    let mut generated_node = grammar.gen_rule(start_rule, depth).unwrap();
+
+    generated_node.simplify_triple();
+
+    let formula = format!("{}", generated_node);
+    let closure_tree = ClosureTree::from_node(&generated_node);
+
+    let rgb_fn = move |coord: PixelCoordinates| closure_tree.eval_rgb(coord.x, coord.y);
+    let img = render_pixels(rgb_fn, width, height);
+
+    let output_img_filepath = get_output_path(&output_img_filename);
+    img.save(output_img_filepath.clone()).expect("failed to save the image");
+    let output_formula_filepath = get_output_path(&output_formula_filename);
+    std::fs::write(output_formula_filepath, formula).unwrap();
+}
---
+++ randomart_standalone/src/utils.rs
@@ -0,0 +1,76 @@
+use image::{ImageBuffer, RgbImage};
+
+pub struct PixelCoordinates {
+    pub x: f32,
+    pub y: f32
+}
+
+pub struct Colour {
+    pub r: f32,
+    pub g: f32,
+    pub b: f32
+}
+
+pub fn render_pixels<F>(function: F, width: u32, height: u32) -> RgbImage 
+where
+    F: Fn(PixelCoordinates) -> Colour 
+{
+    let mut img: RgbImage = ImageBuffer::new(width, height);
+
+    for (px, py, pixel) in img.enumerate_pixels_mut() {
+        let x = (px as f32 / (width - 1) as f32) * 2.0 - 1.0;
+        let y = (py as f32 / (height - 1) as f32) * 2.0 - 1.0;
+
+        let colour = function(PixelCoordinates { x, y });
+
+        let r = ((colour.r + 1.0) * 127.5).clamp(0.0, 255.0) as u8;
+        let g = ((colour.g + 1.0) * 127.5).clamp(0.0, 255.0) as u8;
+        let b = ((colour.b + 1.0) * 127.5).clamp(0.0, 255.0) as u8;
+
+        *pixel = image::Rgb([r, g, b]);
+    }
+    img
+}
+
+pub fn fnv1a(input: &str) -> u64 {
+    let mut hash: u64 = 0xcbf29ce484222325; 
+    let prime: u64 = 0x100000001b3;
+
+    for byte in input.bytes() {
+        hash ^= byte as u64;
+        hash = hash.wrapping_mul(prime); 
+    }
+
+    hash
+}
+
+pub struct LinearCongruentialGenerator {
+    state: u64, 
+    a: u64,    
+    c: u64,   
+    m: u64,    
+}
+
+impl LinearCongruentialGenerator {
+    pub fn new(seed: u64) -> Self {
+        Self {
+            state: seed,
+            a: 1664525,
+            c: 1013904223,
+            m: 2_u64.pow(32), 
+        }
+    }
+
+    pub fn next(&mut self) -> u64 {
+        self.state = (self.a.wrapping_mul(self.state).wrapping_add(self.c)) % self.m;
+        self.state
+    }
+
+    pub fn next_float(&mut self) -> f32 {
+        (self.next() as f32) / (self.m as f32)
+    }
+
+    pub fn next_range(&mut self, min: u64, max: u64) -> u64 {
+        min + (self.next() % (max - min))
+    }
+}
